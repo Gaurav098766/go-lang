d2c5bbd9d5e234d043ee64bf109d9e5107d946a8
Gaurav <gaurav1223344@gmail.com>
2023-05-28 20:07:22 +0530

    maps into action

---
+++ maps/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
